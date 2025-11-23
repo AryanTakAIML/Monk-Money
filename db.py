103cfba7291a6e23f407d84f40caafc45d1c447d
AryanTakAIML <aryan.25bai10508@vitbhopal.ac.in>
2025-11-23 21:50:33 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,26 +1,17 @@
 import sqlite3
 
-# Create database connection
 def get_db():
-    conn = sqlite3.connect("money.db")
-    conn.row_factory = sqlite3.Row
-    return conn
+    con = sqlite3.connect("money.db")
+    con.row_factory = sqlite3.Row
+    return con
 
-# Create table if not exists
 def init_db():
-    conn = get_db()
-    cursor = conn.cursor()
+    con = get_db()
+    cur = con.cursor()
 
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS transactions (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            type TEXT NOT NULL,
-            category TEXT NOT NULL,
-            note TEXT,
-            amount REAL NOT NULL,
-            date TEXT NOT NULL
-        )
-    """)
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT, category TEXT, note TEXT, amount REAL, date TEXT)"
+    )
 
-    conn.commit()
-    conn.close()
+    con.commit()
+    con.close()
